b012c00743e51842a64e784d649b00ef852becf0
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-05-13 17:36:06 +0200

    ./format.sh OK.

--- format.sh
+++ format.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-iconv -f utf8 -t ascii//TRANSLIT < $1 > ${1%.*}_noaccent.txt
-awk 'length($0) > 2' ${1%.*}_noaccent.txt | awk 'length($0) < 17' > ${1%.*}_length.txt
-sed '/[^[:alpha:]]/d' ${1%.*}_length.txt
-#| tr a-z A-Z > ${1%.*}_nospecial_upper.txt
+iconv -f utf8 -t ascii//TRANSLIT < $1 | awk 'length($0) > 2' | awk 'length($0) < 18' | tr a-z A-Z > ${1%.*}_format.txt
